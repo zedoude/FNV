24685ba078ca48164ce98198a093ff92610bc46f
Cédric Ponchy <cedric.ponchy@gmail.com>
2016-07-29 18:22:01 +0200

    Initial Commit

---
+++ FNV/FNV.h
@@ -0,0 +1,19 @@
+//
+//  FNV.h
+//  FNV
+//
+//  Created by Cédric Ponchy on 29/07/16.
+//  Copyright © 2016 Cédric Ponchy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FNV.
+FOUNDATION_EXPORT double FNVVersionNumber;
+
+//! Project version string for FNV.
+FOUNDATION_EXPORT const unsigned char FNVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FNV/PublicHeader.h>
+
+
